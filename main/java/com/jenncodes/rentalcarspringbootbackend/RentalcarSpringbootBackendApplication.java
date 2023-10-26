4a8ce478f18d14d94d7b54011500ad4917c74328
Jenn <jenniferjuarezjj@gmail.com>
2023-10-26 15:39:58 -0400

    create the get and post method to show in both mysql database and postman

--- main/java/com/jenncodes/rentalcarspringbootbackend/RentalcarSpringbootBackendApplication.java
+++ main/java/com/jenncodes/rentalcarspringbootbackend/RentalcarSpringbootBackendApplication.java
@@ -1,13 +1,42 @@
 package com.jenncodes.rentalcarspringbootbackend;
 
+import com.jenncodes.rentalcarspringbootbackend.model.RentalVehicle;
+import com.jenncodes.rentalcarspringbootbackend.repository.RentalVehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class RentalcarSpringbootBackendApplication {
+public class RentalcarSpringbootBackendApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RentalcarSpringbootBackendApplication.class, args);
 	}
 
+	@Autowired
+	private RentalVehicleRepository rentalVehicleRepository;
+
+	@Override
+	public void run(String... args) throws Exception{
+		RentalVehicle rentalVehicle = new RentalVehicle();
+		rentalVehicle.setColor("red");
+		rentalVehicle.setVehicleType("Motorcycle");
+		rentalVehicle.setMake("Kawasaki");
+		rentalVehicle.setYear("2023");
+		rentalVehicle.setMileage("1,100");
+		rentalVehicle.setModel("Ninja 650");
+
+		RentalVehicle rentalVehicle1 = new RentalVehicle();
+		rentalVehicle1.setColor("black");
+		rentalVehicle1.setVehicleType("Sedan");
+		rentalVehicle1.setMake("Honda");
+		rentalVehicle1.setYear("2023");
+		rentalVehicle1.setMileage("5,003");
+		rentalVehicle1.setModel("Civic");
+
+		rentalVehicleRepository.save(rentalVehicle);
+		rentalVehicleRepository.save(rentalVehicle1);
+	}
 }
--- main/java/com/jenncodes/rentalcarspringbootbackend/controller/RentalVehicleController.java
+++ main/java/com/jenncodes/rentalcarspringbootbackend/controller/RentalVehicleController.java
@@ -1,13 +1,39 @@
 package com.jenncodes.rentalcarspringbootbackend.controller;
 
 
+import com.jenncodes.rentalcarspringbootbackend.exception.ResourceNotFoundException;
+import com.jenncodes.rentalcarspringbootbackend.model.RentalVehicle;
+import com.jenncodes.rentalcarspringbootbackend.repository.RentalVehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/rental/vehicles")
 public class RentalVehicleController {
+    @Autowired
+    private RentalVehicleRepository rentalVehicleRepository;
+
+    @GetMapping
+    public List<RentalVehicle> getAllRentalVehicles(){
+        return rentalVehicleRepository.findAll();
+    }
+
+    //create post mapping for rental vehicle
+    @PostMapping
+    public RentalVehicle createRentalVehicle(@RequestBody RentalVehicle rentalVehicle){
+        return rentalVehicleRepository.save(rentalVehicle);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<RentalVehicle> getRentalVehicleById(@PathVariable long id){
+        RentalVehicle rentalVehicle = rentalVehicleRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Vehicle with this id does not exist:" + id));
+        return ResponseEntity.ok(rentalVehicle);
+    }
 
 
 }
