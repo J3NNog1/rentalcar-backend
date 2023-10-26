5c560dbd6c8a86a459578f95db74c915e4868ab9
Jenn <jenniferjuarezjj@gmail.com>
2023-10-26 14:15:02 -0400

    initial commit

---
+++ main/java/com/jenncodes/rentalcarspringbootbackend/RentalcarSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jenncodes.rentalcarspringbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalcarSpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalcarSpringbootBackendApplication.class, args);
+	}
+
+}
---
+++ test/java/com/jenncodes/rentalcarspringbootbackend/RentalcarSpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenncodes.rentalcarspringbootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalcarSpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
