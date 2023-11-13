4b16a5c8758585a60a88b1dae3e729a4a96ecf19
Jenn <jenniferjuarezjj@gmail.com>
2023-11-13 00:35:40 -0500

    add validation to billing.java

--- main/java/com/jenncodes/rentalcarspringbootbackend/model/Billing.java
+++ main/java/com/jenncodes/rentalcarspringbootbackend/model/Billing.java
@@ -7,10 +7,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+
+
 
 @Getter
 @Setter
@@ -24,14 +30,24 @@ public class Billing {
 
     @Column(name= "id")
     private Long id;
+
+    @NotBlank(message = "Payment type is required")
     @Column(name= "payment_type")
     private String paymentType;
+
+    @Positive(message = "Amount must be greater than 0")
     @Column(name= "late_fee")
     private double lateFee;
+
+    @Positive(message = "Total amount must be positive")
     @Column(name= "total_amount")
     private double totalAmount;
+
+    @NotBlank(message = "Bill status is required")
     @Column(name= "bill_status")
     private String billStatus;
+
+    @NotNull(message = "Bill date is required")
     @Column(name= "bill_date")
     private String billDate;
 
