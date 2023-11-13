bfa143f88717f6a8629d8ddfd589e067b40e462b
Jenn <jenniferjuarezjj@gmail.com>
2023-11-13 00:38:26 -0500

    create validation for customer.java

--- main/java/com/jenncodes/rentalcarspringbootbackend/model/Customer.java
+++ main/java/com/jenncodes/rentalcarspringbootbackend/model/Customer.java
@@ -1,6 +1,8 @@
 package com.jenncodes.rentalcarspringbootbackend.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,14 +21,24 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name= "id")
     private long id;
+
+    @NotBlank(message = "First name is required")
     @Column(name= "first_name")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
     @Column(name= "last_name")
     private String lastName;
+
+    @Positive(message = "Please enter a valid phone number")
     @Column(name= "phone_number")
     private int phoneNumber;
+
+    @Positive(message = "Please enter a valid drivers license number")
     @Column(name="drivers_license_number")
     private int driversLicenseNumber;
+
+
     @OneToMany(mappedBy = "customer")
     private List<Reservation> reservations;
 
