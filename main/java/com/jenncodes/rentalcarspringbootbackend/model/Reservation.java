9eb4c72405444402ad613cfa83629099d5f08a15
Jenn <jenniferjuarezjj@gmail.com>
2023-11-13 00:43:19 -0500

    create validation for Reservation.java

--- main/java/com/jenncodes/rentalcarspringbootbackend/model/Reservation.java
+++ main/java/com/jenncodes/rentalcarspringbootbackend/model/Reservation.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import jakarta.validation.constraints.NotNull;
 import java.util.Date;
 
 @Getter
@@ -22,20 +23,25 @@ public class Reservation {
     @Column(name = "id")
     private Long id;
 
+    @NotNull(message = "Customer is required")
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private Customer customer;
 
+    @NotNull(message = "Vehicle is required")
     @ManyToOne
     @JoinColumn(name = "vehicle_id")
     private RentalVehicle vehicle;
 
+    @NotNull(message = "Reservation date is required")
     @Column(name = "reservation_date")
     private Date reservationDate;
 
+    @NotNull(message = "Start date is required")
     @Column(name = "start_date")
     private Date startDate;
 
+    @NotNull(message = "End date is required")
     @Column(name = "end_date")
     private Date endDate;
 
